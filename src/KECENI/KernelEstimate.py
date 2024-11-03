cee8fd27c178556d83b1dd77824a7562760066ee
HeejongBong <hbong@umich.edu>
2024-11-03 14:24:45 -0500

    parallelize bb_bst

--- src/KECENI/KernelEstimate.py
+++ src/KECENI/KernelEstimate.py
@@ -3,6 +3,7 @@ import numpy.random as random
 import pandas as pd
 
 from .Data import Data
+from . import istarmap
 
 class KernelEstimate:
     def __init__(self, fit, i0s, T0s, G0, lamdas, Ds):
@@ -25,7 +26,23 @@ class KernelEstimate:
             * self.ws, 0
         ) / np.sum(self.ws, 0)
     
-    def bb_bst(self, hops=1, n_bst=100, tqdm=None, level_tqdm=0):
+    def nuisance_bst_j(self, j, Xs_bst):
+        ms_bst = np.mean(
+            self.fit.mu(self.fit.data.Ts[self.fit.data.G.N1(j)], Xs_bst, 
+                        self.fit.data.G.sub(self.fit.data.G.N2(j))),
+            -1
+        )
+        
+        vps_bst = np.mean(
+            self.fit.pi(self.fit.data.Ts[self.fit.data.G.N1(j)], Xs_bst, 
+                        self.fit.data.G.sub(self.fit.data.G.N2(j))),
+            -1
+        )
+        
+        return ms_bst, vps_bst
+        
+    
+    def bb_bst(self, hops=1, n_bst=100, n_process=1, tqdm=None, level_tqdm=0):
         if self.fit.data.G.dist is None:
             self.fit.data.G.get_dist()
             
@@ -40,26 +57,51 @@ class KernelEstimate:
             for Xs_i in self.fit.rX(n_bst, np.arange(self.fit.data.n_node), self.fit.data.G)
         ]
         
-        ms_bst = np.zeros((self.fit.data.n_node, n_bst))
-        vps_bst = np.zeros((self.fit.data.n_node, n_bst))
-        for j in tqdm(np.arange(self.fit.data.n_node), smoothing=0, 
-                      desc='bst_j', leave=None, position=level_tqdm):
-            Xs_bst = np.array([
-                cov_fit.sample(self.fit.n_X, self.fit.data.G.N2(j), self.fit.data.G)
-                for cov_fit in cov_fits
-            ])
+#         ms_bst = np.zeros((self.fit.data.n_node, n_bst))
+#         vps_bst = np.zeros((self.fit.data.n_node, n_bst))
+#         for j in tqdm(np.arange(self.fit.data.n_node), smoothing=0, 
+#                       desc='bst_j', leave=None, position=level_tqdm):
+#             Xs_bst = np.array([
+#                 cov_fit.sample(self.fit.n_X, self.fit.data.G.N2(j), self.fit.data.G)
+#                 for cov_fit in cov_fits
+#             ])
 
-            ms_bst[j] = np.mean(
-                self.fit.mu(self.fit.data.Ts[self.fit.data.G.N1(j)], Xs_bst, 
-                            self.fit.data.G.sub(self.fit.data.G.N2(j))),
-                -1
-            )
+#             ms_bst[j] = np.mean(
+#                 self.fit.mu(self.fit.data.Ts[self.fit.data.G.N1(j)], Xs_bst, 
+#                             self.fit.data.G.sub(self.fit.data.G.N2(j))),
+#                 -1
+#             )
 
-            vps_bst[j] = np.mean(
-                self.fit.pi(self.fit.data.Ts[self.fit.data.G.N1(j)], Xs_bst, 
-                            self.fit.data.G.sub(self.fit.data.G.N2(j))),
-                -1
-            )
+#             vps_bst[j] = np.mean(
+#                 self.fit.pi(self.fit.data.Ts[self.fit.data.G.N1(j)], Xs_bst, 
+#                             self.fit.data.G.sub(self.fit.data.G.N2(j))),
+#                 -1
+#             )
+
+        # def nuisance_bst_j(self, j, Xs_bst):
+        
+        if n_process == 1:
+            from itertools import starmap
+            r = list(tqdm(starmap(self.nuisance_bst_j, map(
+                lambda j: (j, np.array([
+                    cov_fit.sample(self.fit.n_X, self.fit.data.G.N2(j), self.fit.data.G)
+                    for cov_fit in cov_fits
+                ])),
+                range(self.fit.data.n_node)
+            )), total=self.fit.data.n_node, leave=None, position=level_tqdm, desc='xi_j', smoothing=0))
+        
+        elif n_process > 1:
+            from multiprocessing import Pool
+            with Pool(n_process) as p:   
+                r = list(tqdm(p.istarmap(self.nuisance_bst_j, map(
+                    lambda j: (j, np.array([
+                        cov_fit.sample(self.fit.n_X, self.fit.data.G.N2(j), self.fit.data.G)
+                        for cov_fit in cov_fits
+                    ])),
+                    range(self.fit.data.n_node)
+                )), total=self.fit.data.n_node, leave=None, position=level_tqdm, desc='xi_j', smoothing=0))
+                
+        ms_bst, vps_bst = list(zip(*r))
             
         xis_bst = (
             (self.fit.data.Ys[:,None] - self.fit.mus[:,None])
